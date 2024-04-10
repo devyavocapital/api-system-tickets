1e3d17936cf8d82ed60bb7ae3a50802b741b8599
devyavocapital <sguadarrama@yavocapital.com>
2024-04-10 11:55:38 -0600

    Feature (email) function to send email with resend dependency and check the auth into middleware

--- models/issues.js
+++ models/issues.js
@@ -1,126 +1,147 @@
-import mongoose from "mongoose";
-import { urlApi } from "../db/config.js";
-import issue from "../schemas/issue.js";
+import mongoose from 'mongoose'
+import { urlApi } from '../db/config.js'
+import issue from '../schemas/issue.js'
+import user from '../schemas/user.js'
+import { getEmailTest } from './email.js'
 
 export class IssuesModel {
-	static async getIssues({ id = "null", nameClient = "null" }) {
-		try {
-			await mongoose.connect(urlApi);
+  static async getIssues({ id = 'null', nameClient = 'null' }) {
+    try {
+      await mongoose.connect(urlApi)
 
-			if (id !== "null") {
-				const issuesList = await issue.findById(id).sort({ created_At: -1 });
-				await mongoose.disconnect();
-				return issuesList;
-			}
+      if (id !== 'null') {
+        const issuesList = await issue.findById(id).sort({ created_At: -1 })
+        await mongoose.disconnect()
+        return issuesList
+      }
 
-			if (id === "null" && nameClient !== "null") {
-				const issuesList = await issue
-					.find({
-						nameClient: { $regex: nameClient },
-					})
-					.sort({ created_At: -1, status: 1, nameClient: 1 });
-				await mongoose.disconnect();
-				return issuesList;
-			}
+      if (id === 'null' && nameClient !== 'null') {
+        const issuesList = await issue
+          .find({
+            nameClient: { $regex: nameClient },
+          })
+          .sort({ created_At: -1, status: 1, nameClient: 1 })
+        await mongoose.disconnect()
+        return issuesList
+      }
 
-			const issuesList = await issue.find().sort({ created_At: -1 });
-			await mongoose.disconnect();
+      const issuesList = await issue.find().sort({ created_At: -1 })
+      await mongoose.disconnect()
 
-			return issuesList;
-		} catch (error) {
-			console.log(error);
-			return { error: "Hubo un error" };
-		}
-	}
+      return issuesList
+    } catch (error) {
+      console.log(error)
+      return { error: 'Hubo un error' }
+    }
+  }
 
-	static async createIssue({
-		nameClient,
-		lastnameClient = "null",
-		motherLastnameClient = "null",
-		creditNumber,
-		socialNumber,
-		cardNumber,
-		initialComment = "",
-		assignTo,
-		nameAssignated,
-		status = "pendient",
-		category = 0,
-		daysConfig,
-		userId,
-	}) {
-		try {
-			await mongoose.connect(urlApi);
+  static async createIssue({
+    nameClient,
+    lastnameClient = 'null',
+    motherLastnameClient = 'null',
+    creditNumber,
+    socialNumber,
+    cardNumber,
+    initialComment = '',
+    assignTo = null,
+    nameAssignated,
+    status = 'pendient',
+    category = 0,
+    daysConfig,
+    userId,
+  }) {
+    try {
+      await mongoose.connect(urlApi)
 
-			const newIssue = issue({
-				nameClient,
-				lastnameClient,
-				motherLastnameClient,
-				creditNumber,
-				socialNumber,
-				cardNumber,
-				initialComment,
-				assignTo,
-				nameAssignated,
-				status,
-				category,
-				daysConfig,
-				userId,
-			});
+      const newIssue = issue({
+        nameClient,
+        lastnameClient,
+        motherLastnameClient,
+        creditNumber,
+        socialNumber,
+        cardNumber,
+        initialComment,
+        assignTo,
+        nameAssignated,
+        status,
+        category,
+        daysConfig,
+        userId,
+      })
 
-			const issueAdded = await newIssue.save();
-			await mongoose.disconnect();
-			return { msg: "Incidencia creada correctamente", issueAdded };
-		} catch (error) {
-			console.log(error);
-			return { error };
-		}
-	}
+      const issueAdded = await newIssue.save()
 
-	static async updateIssue({
-		id,
-		nameClient,
-		lastnameClient = "null",
-		motherLastnameClient = "null",
-		creditNumber,
-		socialNumber,
-		cardNumber,
-		assignTo,
-		nameAssignated,
-		status = "pendient",
-		category = 0,
-		daysConfig,
-	}) {
-		try {
-			await mongoose.connect(urlApi);
+      if (assignTo !== null || assignTo !== undefined) {
+        const userAssignated = await user.findById({ _id: assignTo })
+        getEmailTest({
+          toEmail: userAssignated.email,
+          subject: `Ticket Asignado - ${nameClient}`,
+          nameClient,
+        })
+      }
 
-			const issueExist = await issue.findById(id);
-			if (!issueExist) {
-				return {
-					error: "Error: No existe incidencia con este ID.",
-					status: 401,
-				};
-			}
+      await mongoose.disconnect()
+      return { msg: 'Incidencia creada correctamente', issueAdded }
+    } catch (error) {
+      console.log(error)
+      return { error }
+    }
+  }
 
-			await issue.findByIdAndUpdate(id, {
-				nameClient,
-				lastnameClient,
-				motherLastnameClient,
-				creditNumber,
-				socialNumber,
-				cardNumber,
-				assignTo,
-				nameAssignated,
-				status,
-				category: parseInt(category),
-				daysConfig: parseInt(daysConfig),
-			});
+  static async updateIssue({
+    id,
+    nameClient,
+    lastnameClient = 'null',
+    motherLastnameClient = 'null',
+    creditNumber,
+    socialNumber,
+    cardNumber,
+    assignTo = null,
+    nameAssignated,
+    status = 'pendient',
+    category = 0,
+    daysConfig,
+  }) {
+    try {
+      await mongoose.connect(urlApi)
 
-			await mongoose.disconnect();
+      const issueExist = await issue.findById(id)
+      if (!issueExist) {
+        return {
+          error: 'Error: No existe incidencia con este ID.',
+          status: 401,
+        }
+      }
 
-			return { msg: "Incidencia actualizada correctamente", status: 200 };
-		} catch (error) {
-			console.log(error);
-			return { error };
-		}
-	}
+      await issue.findByIdAndUpdate(id, {
+        nameClient,
+        lastnameClient,
+        motherLastnameClient,
+        creditNumber,
+        socialNumber,
+        cardNumber,
+        assignTo,
+        nameAssignated,
+        status,
+        category: parseInt(category),
+        daysConfig: parseInt(daysConfig),
+      })
+
+      if (assignTo !== null || assignTo !== undefined) {
+        const userAssignated = await user.findById({ _id: assignTo })
+        getEmailTest({
+          toEmail: userAssignated.email,
+          subject: `Ticket Asignado - ${nameClient}`,
+          nameClient,
+        })
+      }
+
+      await mongoose.disconnect()
+
+      return { msg: 'Incidencia actualizada correctamente', status: 200 }
+    } catch (error) {
+      console.log(error)
+      return { error }
+    }
+  }
 }
